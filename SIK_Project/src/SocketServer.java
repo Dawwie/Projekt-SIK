7d1af7fa27b2aab848c456af03e7c3a5a98f0abb
Dawwie <32976041+Dawwie@users.noreply.github.com>
2019-01-29 19:08:18 +0100

    Dodane mapowanie klientów

--- SIK_Project/src/SocketServer.java
+++ SIK_Project/src/SocketServer.java
@@ -1,32 +1,40 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SocketServer {
     int port = 44444;
-    int clients = 0;
+    public static int clients = 1;
     ServerSocket serverSocket = null;
-
+    public static Map<Integer, Player> players;
+    public static boolean allPlayers = false;
+    public static int random = ThreadLocalRandom.current().nextInt(1, 5);
     public void runServer() { //create new server socket
+    players = new HashMap<>();
         try {
             serverSocket = new ServerSocket(port);
-            while (clients!=5) {//searching for connections
+            while(clients != 6){//searching for connections
                 try{
                     Socket clientSocket = serverSocket.accept();//every time the ServerSocket() accept connection form the client
                     RunnableServer r = new RunnableServer(clientSocket);//this is storing in clientSocket
                     new Thread(r).start();
                     clients++;
-                    if(clients == 5){
+                    if(clients == 6){
                         serverSocket.close();
+                        allPlayers = true;
                     }
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 }//end 2. try }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }//end 1. try
-
-
     }// end run method
 }
